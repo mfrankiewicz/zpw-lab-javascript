42f1a82dcb62ef1680b08e25a2baa7401df45a13
Michał Frankiewicz <frankiewiczmh@gmail.com>
2016-11-27 16:13:41 +0100

    dodaje listę produktów z paginacją oraz filtrem

--- src/lab05/angular/controllers.js
+++ src/lab05/angular/controllers.js
@@ -24,12 +24,99 @@ appControllers.controller('mixedTasksCtrl', function($scope) {
 
 appControllers.controller('shopCtrl', function($scope) {
 
+    $scope.categories = [
+        {
+            id: 0,
+            name: 'wybierz'
+        },
+        {
+            id: 1,
+            name: 'narzędzia'
+        },
+        {
+            id: 2,
+            name: 'AGD'
+        },
+        {
+            id: 3,
+            name: 'elektronika'
+        }
+    ];
+    $scope.products = [
+        {
+            id: 1,
+            name: 'Młotek',
+            price: 20.11,
+            categoryId: 1
+        },
+        {
+            id: 2,
+            name: 'Odtwarzacz MP3',
+            price: 99.99,
+            categoryId: 3
+        },
+        {
+            id: 3,
+            name: 'Toster',
+            price: 25.30,
+            categoryId: 2
+        },
+        {
+            id: 4,
+            name: 'Monitor',
+            price: 999.99,
+            categoryId: 3
+        },
+        {
+            id: 5,
+            name: 'Śrubokręt',
+            price: 5.32,
+            categoryId: 1
+        },
+        {
+            id: 6,
+            name: 'Gofrownica',
+            price: 75.49,
+            categoryId: 2
+        }
+    ];
+    $scope.currentPage = 0;
+    $scope.pageSize = 3;
+    $scope.selectedCategory = $scope.categories[0];
 
+    $scope.numberOfPages = function(){
+        console.log($scope.getProductCountByCategoryId($scope.selectedCategory.id));
+        return Math.ceil($scope.getProductCountByCategoryId($scope.selectedCategory.id)/$scope.pageSize);
+    }
 
+    $scope.getCategoryName = function(categoryId) {
+        var result = '';
+        angular.forEach($scope.categories, function(category, key) {
+            if (category.id == categoryId) {
+                result = category.name;
+            }
+        });
 
+        return result;
+    };
+
+    $scope.getProductCountByCategoryId = function(categoryId) {
+        var count = 0;
+        if (!categoryId) {
+            return $scope.products.length;
+        }
+
+        angular.forEach($scope.products, function(product, key) {
+            if (product.categoryId == categoryId) {
+                count++;
+            }
+        });
+
+        return count;
+    };
 });
 
-appControllers.controller('cartCtrl', function($scope) {
+appControllers.controller('cartCtrl', function($scope, cartService) {
 
 
 
--- src/lab05/angular/filters.js
+++ src/lab05/angular/filters.js
@@ -1,1 +1,24 @@
 var appFilters = angular.module('appFilters', []);
+
+appFilters.filter('startFrom', function() {
+    return function(input, start) {
+        start = +start;
+        return input.slice(start);
+    }
+});
+
+appFilters.filter('categoryFilter', function() {
+    return function(input, category) {
+        var result = [];
+        if (category.id == 0) {
+            return input;
+        }
+        angular.forEach(input, function(product, key) {
+            if (product.categoryId == category.id) {
+                result.push(product);
+            }
+        });
+
+        return result;
+    }
+});
