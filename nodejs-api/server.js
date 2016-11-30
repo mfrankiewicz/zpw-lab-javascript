c55aa5c61eb7bbc9e84ac2c4f612fddf7072de19
Michał Frankiewicz <frankiewiczmh@gmail.com>
2016-12-01 00:56:04 +0100

    dodaje obraz nodejs

---
+++ nodejs-api/server.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const express = require('express');
+
+// Constants
+const PORT = 8080;
+
+// App
+const app = express();
+app.get('/', function (req, res) {
+  res.send('api.zpw.loc');
+});
+
+app.listen(PORT);
