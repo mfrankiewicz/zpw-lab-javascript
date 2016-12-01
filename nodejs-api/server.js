adae9e39bbfa7827e2d2076f321cf9793c22a22d
Michał Frankiewicz <frankiewiczmh@gmail.com>
2016-12-01 02:24:51 +0100

    dodaje potrzebne endpointy

--- nodejs-api/server.js
+++ nodejs-api/server.js
@@ -1,14 +1,55 @@
 'use strict';
 
+// constants
 const express = require('express');
+const mongoose = require('mongoose');
 
-// Constants
-const PORT = 8080;
-
-// App
 const app = express();
+
+mongoose.connect('mongodb://172.16.241.1:27017/db');
+
 app.get('/', function (req, res) {
   res.send('api.zpw.loc');
 });
 
-app.listen(PORT);
+/**
+ * /products
+ */
+app.get('/products', function (req, res) {
+
+})
+app.post('/products', function (req, res) {
+
+})
+app.put('/products', function (req, res) {
+
+})
+app.delete('/products', function (req, res) {
+
+})
+
+/**
+ * /product-categories
+ */
+app.get('/product-categories', function (req, res) {
+
+})
+
+/**
+ * /orders
+ */
+app.get('/orders', function (req, res) {
+
+})
+app.post('/orders', function (req, res) {
+
+})
+
+/**
+ * /users
+ */
+app.post('/users/login', function (req, res) {
+
+})
+
+app.listen(8080);
